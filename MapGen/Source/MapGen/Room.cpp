8ba2718fc5bacecae98dd3d7c6b886e0f04752d7
Jacob Pozaic <JacobPozaic@users.noreply.github.com>
2017-10-11 01:21:40 -0400

    Updated commenting

--- MapGen/Source/MapGen/Room.cpp
+++ MapGen/Source/MapGen/Room.cpp
@@ -1,8 +1,12 @@
 #include "Room.h"
 
+/* Init static constants */
 int Room::ROOM_SIZE;
-int Room::TILE_SIZE;
+int Room::TILE_SIZE;	
 float Room::DOOR_WIDTH;
+
+int Room::EDGE_OFFSET = (ROOM_SIZE / 2) - (TILE_SIZE / 2);
+int Room::DOOR_OFF_RNG = (ROOM_SIZE / TILE_SIZE) / 2 - 4;	// doors are 4 tiles wide + keeps doors atleast 2 tiles from a corner
 
 Room::Room(int x, int y, int dir, Room* last) {
 	this->x = x;
@@ -10,63 +14,60 @@ Room::Room(int x, int y, int dir, Room* last) {
 	this->dir = dir;
 	this->last = last;
 
-	EDGE_OFFSET = (ROOM_SIZE / 2) - (TILE_SIZE/2);
-	DOOR_OFF_RNG = (ROOM_SIZE / TILE_SIZE) / 2 - 4; // doors are 4 tiles wide + keeps doors atleast 2 tiles from a corner
+	this->worldX = x * ROOM_SIZE;				// calculate this rooms position in world space
+	this->worldY = y * ROOM_SIZE;				// calculate this rooms position in world space
 
-	this->worldX = x * ROOM_SIZE;
-	this->worldY = y * ROOM_SIZE;
-
-	left = ROOM_SIZE + worldY + DOOR_WIDTH;
-	above = ROOM_SIZE + worldX + DOOR_WIDTH;
-	right = ROOM_SIZE + worldY + DOOR_WIDTH;
-	below = ROOM_SIZE + worldX + DOOR_WIDTH;
+	left = ROOM_SIZE + worldY + DOOR_WIDTH;		// simulate no door (distance is always larger than distance to a door, which would prevent a wall from spawning there)
+	above = ROOM_SIZE + worldX + DOOR_WIDTH;	// simulate no door (distance is always larger than distance to a door, which would prevent a wall from spawning there)
+	right = ROOM_SIZE + worldY + DOOR_WIDTH;	// simulate no door (distance is always larger than distance to a door, which would prevent a wall from spawning there)
+	below = ROOM_SIZE + worldX + DOOR_WIDTH;	// simulate no door (distance is always larger than distance to a door, which would prevent a wall from spawning there)
 }
 
 void Room::addDoor() {
-	Coord doorPos;
-	doorPos.x = 0;
-	doorPos.y = 0;
-	int door_offset = FMath::RandRange(-DOOR_OFF_RNG, DOOR_OFF_RNG) * TILE_SIZE;
-	switch (this->dir) {
-	case 0: 
-		doorPos.x -= EDGE_OFFSET;
-		doorPos.y += door_offset;
-		left = doorPos.y + worldY;
+	Coord doorPos;								// stores the location of a door in world space
+	doorPos.x = 0;								// coordinates allways start at 0
+	doorPos.y = 0;								// coordinates allways start at 0
+	int door_offset = FMath::RandRange(-DOOR_OFF_RNG, DOOR_OFF_RNG) * TILE_SIZE;	// pick a random tile to shift the door by (so doors are not always centered on each wall)
+	switch (this->dir) {						// determine what wall the door is on
+	case 0:										// if the door is on the left wall
+		doorPos.x -= EDGE_OFFSET;				// set its x position to be on the left wall
+		doorPos.y += door_offset;				// set its y position to be a random spot along the left wall
+		left = doorPos.y + worldY;				// cache the offset location of this door in world cordinates to be used for wall placement
 		break;
-	case 1:	
-		doorPos.y -= EDGE_OFFSET;
-		doorPos.x += door_offset;
-		above = doorPos.x + worldX;
+	case 1:										// if the door is on the above wall	
+		doorPos.y -= EDGE_OFFSET;				// set its y position to be on the above wall
+		doorPos.x += door_offset;				// set its x position to be a random spot along the above wall
+		above = doorPos.x + worldX;				// cache the offset location of this door in world cordinates to be used for wall placement
 		break;
-	case 2:	
-		doorPos.x += EDGE_OFFSET;
-		doorPos.y += door_offset;
-		right = doorPos.y + worldY;
+	case 2:										// if the door is on the right wall
+		doorPos.x += EDGE_OFFSET;				// set its x position to be on the right wall
+		doorPos.y += door_offset;				// set its y position to be a random spot along the right wall
+		right = doorPos.y + worldY;				// cache the offset location of this door in world cordinates to be used for wall placement
 		break;
-	case 3:	
-		doorPos.y += EDGE_OFFSET;
-		doorPos.x += door_offset;
-		below = doorPos.x + worldX;
+	case 3:										// if the door is on the bottom wall
+		doorPos.y += EDGE_OFFSET;				// set its y position to be on the bottom wall
+		doorPos.x += door_offset;				// set its x position to be a random spot along the bottom wall
+		below = doorPos.x + worldX;				// cache the offset location of this door in world cordinates to be used for wall placement
 		break;
 	}
 	
-	this->doors.push_back(doorPos);
-	last->addDoorEntrance(doorPos);
+	this->doors.push_back(doorPos);				// add the door to the list of doors for this room
+	last->addDoorEntrance(doorPos);				// add a door on the previous room that connects to the new door on this room
 }
 
 void Room::addDoorEntrance(Coord doorPos) {
-	if (abs(doorPos.x) == EDGE_OFFSET)
-		doorPos.x = -doorPos.x;
-	else if (abs(doorPos.y) == EDGE_OFFSET)
-		doorPos.y = -doorPos.y;
+	/* flip the direction of the door so it lines up with the door on the new room */
+	if (abs(doorPos.x) == EDGE_OFFSET) doorPos.x = -doorPos.x;
+	else if (abs(doorPos.y) == EDGE_OFFSET) doorPos.y = -doorPos.y;
 	
+	/* cache the offset location of this door in world cordinates to be used for wall placement */
 	switch (getDoorSide(doorPos)) {
 	case 0: left = doorPos.y + worldY; break;
 	case 1:	above = doorPos.x + worldX; break;
 	case 2:	right = doorPos.y + worldY; break;
 	case 3:	below = doorPos.x + worldX; break;
 	}
-	this->doors.push_back(doorPos);
+	this->doors.push_back(doorPos);				// add the door to the list of doors for this room
 }
 
 int Room::getDoorSide(Coord door) {
@@ -87,46 +88,51 @@ FTransform Room::getWorldPosition() {
 }
 
 std::vector<FTransform> Room::getDoorPositions() {
-	std::vector<FTransform> doorPositions;
-	for (int i = 0; i < this->doors.size(); i++) {
+	std::vector<FTransform> doorPositions;														// stores the FTransform for all doors in this room
+	for (int i = 0; i < this->doors.size(); i++) {												// for every door
 		Coord door = doors[i];
-		FTransform trans_door = FTransform(FVector(door.x + worldX, door.y + worldY, 0));
-		trans_door.SetRotation(FQuat::MakeFromEuler({ 0.f, 0.f, getDoorSide(door) * 90.0f }));
-		doorPositions.push_back(trans_door);
+		FTransform trans_door = FTransform(FVector(door.x + worldX, door.y + worldY, 0));		// create the FTransform using the world coordinates of the door
+		trans_door.SetRotation(FQuat::MakeFromEuler({ 0.f, 0.f, getDoorSide(door) * 90.0f }));	// rotate the door to match the wall it is against
+		doorPositions.push_back(trans_door);													// add the FTransform to the list
 	}
-	return doorPositions;
+	return doorPositions;																		// return the list of FTransforms for all the doors
 }
 
 std::vector<FTransform> Room::getWallPositions() {
 	std::vector<Coord> wallPositions;
+	/* calculates the range of world coordinates that this room occupies */
 	float startX = worldX - EDGE_OFFSET;
 	float endX = worldX + EDGE_OFFSET;
 	float startY = worldY - EDGE_OFFSET;
 	float endY = worldY + EDGE_OFFSET;
 
 	/* Calculate wall positions around the edge of the room */
+	/* This is kindof hard to explain step by step but basically it puts a wall on every tile that is on the edge of room 
+	 * if the distance from the cached door location for that wall and that particular peice of wall is less than the width of the door then dont place that wall peice.
+	 */
 	Coord wall;
 	for (int i = 0; i < ROOM_SIZE; i += TILE_SIZE) {
+		/* place wall peice on the top wall */
 		wall.x = startX + i;
 		wall.y = startY;
-		if(abs(wall.x - above) >= DOOR_WIDTH)
-			wallPositions.push_back(wall);
+		if(abs(wall.x - above) >= DOOR_WIDTH) wallPositions.push_back(wall);
+		/* place wall peice on the bottom wall */
 		wall.x = startX + i;
 		wall.y = endY;
-		if(abs(wall.x - below) >= DOOR_WIDTH)
-			wallPositions.push_back(wall);
+		if(abs(wall.x - below) >= DOOR_WIDTH) wallPositions.push_back(wall);
 	}
-	for (int i = TILE_SIZE; i < ROOM_SIZE - TILE_SIZE; i += TILE_SIZE) {
+	for (int i = TILE_SIZE; i < ROOM_SIZE - TILE_SIZE; i += TILE_SIZE) {		// top/bottom walls cover the corner peices so dont also do corners with the left/right
+		/* place wall peice on the left wall */
 		wall.x = startX;
 		wall.y = startY + i;
-		if (abs(wall.y - left) >= DOOR_WIDTH)
-			wallPositions.push_back(wall);
+		if (abs(wall.y - left) >= DOOR_WIDTH) wallPositions.push_back(wall);
+		/* place wall peice on the right wall */
 		wall.x = endX;
 		wall.y = startY + i;
-		if (abs(wall.y - right) >= DOOR_WIDTH)
-			wallPositions.push_back(wall);
+		if (abs(wall.y - right) >= DOOR_WIDTH) wallPositions.push_back(wall);
 	}
 
+	/* create an FTransform for each wall and return them in a list */
 	std::vector<FTransform> walls;
 	for (int i = 0; i < wallPositions.size(); i++) {
 		Coord wall = wallPositions[i];
--- MapGen/Source/MapGen/Room.h
+++ MapGen/Source/MapGen/Room.h
@@ -6,25 +6,25 @@
  */
 class MAPGEN_API Room {
 private:
-	struct Coord {									// stores the real world coordinates of components in the room, ex: walls, doors
-		float x;									// the x position of the object
-		float y;									// the y position of the object
+	struct Coord {						// stores the real world coordinates of components in the room, ex: walls, doors
+		float x;						// the x position of the object
+		float y;						// the y position of the object
 	};
 
-	int worldX;										// coodinate location of this room in world units
-	int worldY;										// coodinate location of this room in world units
+	int worldX;							// coodinate location of this room in world units
+	int worldY;							// coodinate location of this room in world units
 
-	int EDGE_OFFSET;								// constant value for the offset from the center of a room to the edge
-	int DOOR_OFF_RNG;								// constant value for how far a door can be offset from the center of a wall
+	static int EDGE_OFFSET;				// constant value for the offset from the center of a room to the edge
+	static int DOOR_OFF_RNG;			// constant value for how far a door can be offset from the center of a wall
 
-	Room* last;										// stores a pointer to the previous room in the chain
+	Room* last;							// stores a pointer to the previous room in the chain
 
-	std::vector<Coord> doors;						// stores the location of each door in the room
+	std::vector<Coord> doors;			// stores the location of each door in the room
 
-	float left;
-	float above;
-	float right;
-	float below;
+	float left;							// stores the position of the door on the left wall if there is one
+	float above;						// stores the position of the door on the upper wall if there is one
+	float right;						// stores the position of the door on the right wall if there is one
+	float below;						// stores the position of the door on the lower wall if there is one
 
 	/**
 	 * Gets the side of a room a door is on
@@ -32,12 +32,12 @@ private:
 	int getDoorSide(Coord door);
 
 public:
-	int x;											// coordinate location of this room in relation to other rooms
-	int y;											// coordinate location of this room in relation to other rooms
-	int dir;										// stores the direction to the previous room
+	int x;								// coordinate location of this room in relation to other rooms
+	int y;								// coordinate location of this room in relation to other rooms
+	int dir;							// stores the direction to the previous room
 
-	static int ROOM_SIZE;							// constant value for the size in world units of each room
-	static int TILE_SIZE;							// constant value for the size of each tile in world units
+	static int ROOM_SIZE;				// constant value for the size in world units of each room
+	static int TILE_SIZE;				// constant value for the size of each tile in world units
 	static float DOOR_WIDTH;
 
 	Room(int x = 0, int y = 0, int dir = -1, Room* last = NULL);
@@ -48,7 +48,8 @@ public:
 	void addDoor();
 
 	/**
-	* Add a door between this room and the previous room in the chain
+	* Add a door between this room and the previous room in the chain (only called from other Room objects)
+	* @param doorPos The coordinates of the door on the new room, to be reveresed on this room so the doors line up
 	*/
 	void addDoorEntrance(Coord doorPos);
 
